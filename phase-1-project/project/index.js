e5055de6075bc378b269e9904332ef82032ffc01
bcaudillo <btcaudillo@gmail.com>
2022-08-07 22:25:17 -0600

    create asynchromous request

--- phase-1-project/project/index.js
+++ phase-1-project/project/index.js
@@ -1,1 +1,12 @@
 //I've been workiing on the project and forgot to commit to github while I was coding. Starting commits from where I left off
+function asyncRequest(){
+    fetch("https://rws-cards-api.herokuapp.com/api/v1/cards/")
+        .then(resp=>resp.json())
+        .then(object => {
+            cardsObj = object.cards
+            presentAllCards()
+            chooseSpread(object)
+            setIntention()
+        })
+        
+    }
